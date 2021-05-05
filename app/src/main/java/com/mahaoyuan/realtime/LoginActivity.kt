45d4d01ac4da9652a8ea1c44caa17b8e249f1d4c
haoyuan ma <1440970573@qq.com>
2021-05-06 01:15:41 +0800

    reset: MainActivity

--- app/src/main/java/com/mahaoyuan/realtime/LoginActivity.kt
+++ app/src/main/java/com/mahaoyuan/realtime/LoginActivity.kt
@@ -27,7 +27,6 @@ class LoginActivity : AppCompatActivity() {
                     val intent = Intent(this@LoginActivity,MainActivity::class.java)
                     startActivity(intent)
                 }
-
             }
             super.handleMessage(msg)
         }
@@ -77,7 +76,7 @@ class LoginActivity : AppCompatActivity() {
                 msg.what = 1
                 handler.sendMessage(msg)
             }
-
+            token?.let { Log.i("mhy", it) }
         }
     }
 }
