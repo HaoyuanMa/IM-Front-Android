999726ca1e9ea33493cb50024ef9184e91259683
haoyuan ma <1440970573@qq.com>
2021-05-10 00:16:59 +0800

    add : setOnline,Bind get(remove)Users

--- app/src/main/java/com/mahaoyuan/realtime/UserInfo.kt
+++ app/src/main/java/com/mahaoyuan/realtime/UserInfo.kt
@@ -1,6 +1,8 @@
 package com.mahaoyuan.realtime
 
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.MutableLiveData
 import com.microsoft.signalr.HubConnection
 import com.microsoft.signalr.HubConnectionBuilder
@@ -46,8 +48,58 @@ object UserInfo {
     }
 
     fun StartConnection(){
-        connection.value?.start()
+        val start = connection.value?.start()
+        if (start != null) {
+            start.blockingGet()
+        }
     }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun Bind(){
+        val conn = connection.value
+        if (conn != null) {
+            conn.on("RemoveUser", { user: String ->
+                chatUsers.value?.removeIf { t :String -> t == user  }
+                broadcastUsers.value?.removeIf { t:String -> t == user }
+                chatRoomUsers.value?.removeIf { t:String -> t == user }
+                usersCount.postValue(usersCount.value?.minus(1))
+                Log.i("mhy","remove user")
+            },String::class.java)
+
+            conn.on("GetChatUsers",{users : Any? ->
+                (users as MutableList<String>).forEach {
+                    chatUsers.value?.add(it)
+                    usersCount.postValue(usersCount.value?.plus(1))
+                }
+                Log.i("mhy","get chat users")
+            }, mutableListOf<String>()::class.java)
+
+
+        }
+
+    }
+
+    fun SetOnline(type :String){
+        connection.value?.send("SetOnline",type)
+        Log.i("mhy","setOnline")
+    }
+
+    fun StopConnenction(){
+        chatUsers.value?.clear()
+        broadcastUsers.value?.clear()
+        chatRoomUsers.value?.clear()
+        usersCount.value = 0
+        connection.value?.stop()
+        //todo
+    }
+
+    fun AddUser(item : String){
+        chatUsers.value!!.add(item)
+        broadcastUsers.value!!.add(item)
+        chatRoomUsers.value!!.add(item)
+        usersCount.value = usersCount.value?.plus(1)
+    }
+
 
 
 }
--- app/src/main/java/com/mahaoyuan/realtime/activities/ChatActivity.kt
+++ app/src/main/java/com/mahaoyuan/realtime/activities/ChatActivity.kt
@@ -1,27 +1,33 @@
 package com.mahaoyuan.realtime.activities
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.Observer
 import com.mahaoyuan.realtime.R
 import com.mahaoyuan.realtime.UserInfo
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import okhttp3.internal.wait
 
 class ChatActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
-//        UserInfo.BuildConnection()
-//        UserInfo.StartConnection()
-//        Log.i("mhy","conn start")
-//        UserInfo.connection.value?.connectionState?.let { Log.i("mhy", it.name) }
+        UserInfo.BuildConnection()
+        UserInfo.StartConnection()
+        Log.i("mhy","conn start")
+        UserInfo.Bind()
+        UserInfo.mode.value = "chat"
+        UserInfo.SetOnline("chat")
 
 //        runBlocking {
 //            UserInfo.BuildConnection()
@@ -32,6 +38,7 @@ class ChatActivity : AppCompatActivity() {
 //        }
 
 
+
         val listView = findViewById<ListView>(R.id.chat_users)
         val adapter = UserInfo.chatUsers.value?.let {
             ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,
@@ -40,14 +47,15 @@ class ChatActivity : AppCompatActivity() {
 
         listView.adapter = adapter
         listView.setOnItemClickListener { parent, view, pos, id ->
-            Toast.makeText(this,"click",Toast.LENGTH_SHORT).show()
+            //Toast.makeText(this,"click",Toast.LENGTH_SHORT).show()
+            //todo: open chat acti
             val item = UserInfo.chatUsers.value?.get(pos)
 
             if (item != null) {
-                UserInfo.chatUsers.value!!.add(item)
-                UserInfo.usersCount.value = UserInfo.usersCount.value?.plus(1)
+                UserInfo.AddUser(item)
                 Log.i("mhy",item)
             }
+            //todo
         }
 
         UserInfo.usersCount.observe(this, Observer { users ->
@@ -60,5 +68,11 @@ class ChatActivity : AppCompatActivity() {
         })
 
 
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        UserInfo.StopConnenction()
     }
 }
