5be2ad7c4c7cdad5d93686d39e7ac197f461f6d4
haoyuan ma <1440970573@qq.com>
2021-05-15 13:40:01 +0800

    add:show msg depend on diff user

--- app/src/main/java/com/mahaoyuan/realtime/adapters/MessageAdapter.kt
+++ app/src/main/java/com/mahaoyuan/realtime/adapters/MessageAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.mahaoyuan.realtime.R
+import com.mahaoyuan.realtime.UserInfo
 import com.mahaoyuan.realtime.models.Message
 import java.lang.IllegalArgumentException
 
@@ -15,14 +16,32 @@ class MessageAdapter(var msgList: MutableList<Message>) : RecyclerView.Adapter<R
         val textMsg : TextView = view.findViewById(R.id.msg_content_text)
     }
 
+    inner class EmptyViewHolder(view: View) : RecyclerView.ViewHolder(view){
+
+    }
+
     override fun getItemViewType(position: Int): Int {
         val msg = msgList[position]
-        return when(msg.contentType){
-            "text" -> 1
-            "image" -> 2
-            "file" -> 3
-            else -> 0
+        return if (msg.type == "chat"){
+            if ((msg.from == UserInfo.userEmail.value && msg.to[0] == UserInfo.chatTo.value) || (msg.from == UserInfo.chatTo.value  && msg.to[0] == UserInfo.userEmail.value)){
+                when(msg.contentType){
+                    "text" -> 1
+                    "image" -> 2
+                    "file" -> 3
+                    else -> 0
+                }
+            }else{
+                0
+            }
+        }else{
+            when(msg.contentType){
+                "text" -> 1
+                "image" -> 2
+                "file" -> 3
+                else -> 0
+            }
         }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
@@ -31,8 +50,8 @@ class MessageAdapter(var msgList: MutableList<Message>) : RecyclerView.Adapter<R
             TextViewHolder(view)
         }
         else{
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.message_text,parent,false)
-            TextViewHolder(view)
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.message_empty,parent,false)
+            EmptyViewHolder(view)
         }
     }
 
@@ -42,6 +61,9 @@ class MessageAdapter(var msgList: MutableList<Message>) : RecyclerView.Adapter<R
             is TextViewHolder -> {
                 holder.textMsg.text = ("   " + msg.content)
                 holder.msgFrom.text = (" " + msg.from)
+            }
+            is EmptyViewHolder -> {
+
             }
             else -> throw IllegalArgumentException()
         }
